36e4d4a2c86064c361358d273be6f8ad80c733e5
Yuki-known <yukinon.writes@gmail.com>
2025-06-29 00:37:20 -0400

    removed gh pages setup

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -1,11 +1,11 @@
 import { type RouteConfig, route, index } from "@react-router/dev/routes";
 
 export default [
-    route("/kalatalo", "routes/header.tsx", [
+    route("/", "routes/header.tsx", [
         index("routes/home.tsx"),
-        route("/kalatalo/me", "routes/me.tsx"),
-        route("/kalatalo/research", "routes/research.tsx"),
-        route("/kalatalo/papers", "routes/papers.tsx"),
-        route("/kalatalo/cv", "routes/cv.tsx"),
-    ])
+        route("/me", "routes/me.tsx"),
+        route("/research", "routes/research.tsx"),
+        route("/papers", "routes/papers.tsx"),
+        route("/cv", "routes/cv.tsx"),
+    ]),
 ] satisfies RouteConfig;
